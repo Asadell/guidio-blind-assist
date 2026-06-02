e46572c36aa736e94268463cf88b594434a0fce7
Asadell <m.satrioasadell@gmail.com>
2026-06-02 12:19:40 +0700

    Initial Commit mobile

---
+++ guidio_app/README.md
@@ -0,0 +1,3 @@
+# guidio_app
+
+A new Flutter project.
---
+++ guidio_app/android/app/src/main/kotlin/com/example/guidio_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.guidio_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
